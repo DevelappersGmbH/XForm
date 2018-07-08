d1c9459bbcf6f5c41225838d667539ea043c0ac8
Johannes Starke <johannesstarke@gmail.com>
2018-07-08 18:42:21 +0200

    Basic android sample setup

---
+++ Sample/Sample.AndroidApp/Views/MainView.cs
@@ -0,0 +1,15 @@
+﻿
+using Android.App;
+using Android.OS;
+
+namespace Sample.AndroidApp.Views
+{
+    [Activity(Label = "MainView", MainLauncher = true)]
+    public class MainView : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
