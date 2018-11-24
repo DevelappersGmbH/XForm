f00dac86dcdec3b1abaa69c7258cfdae25c5f56c
Johannes Starke <johannesstarke@gmail.com>
2018-11-24 19:05:31 +0100

    Implemented login sample

--- Sample/Sample.Core/ViewModels/SampleLoginFormViewModel.cs
+++ Sample/Sample.Core/ViewModels/SampleLoginFormViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using MvvmCross.Commands;
+using Sample.Core.Extensions;
 using Sample.Core.ViewModels.Bases;
 using XForm.Fields;
 using XForm.Fields.Interfaces;
@@ -10,20 +11,44 @@ namespace Sample.Core.ViewModels
 {
     public class SampleLoginFormViewModel : FormViewModel
     {
+        private IMvxCommand _loginCommand;
+
+        private readonly EmailAddressTextField _emailAddressField = new EmailAddressTextField("E-Mail Address");
+        private readonly PasswordTextField _passwordField = new PasswordTextField("Password");
+        private ButtonField _buttonField;
+
+        private IMvxCommand LoginCommand =>
+            _loginCommand ?? (_loginCommand = new MvxAsyncCommand(HandleLoginCommand, CanHandleLoginCommand));
+
         public override async Task Initialize()
         {
             await base.Initialize();
-            
+
+            _buttonField = new ButtonField("Login", LoginCommand);
+
+            _emailAddressField.ValueChanged += (sender, args) => LoginCommand.RaiseCanExecuteChanged();
+            _passwordField.ValueChanged += (sender, args) => LoginCommand.RaiseCanExecuteChanged();
+
             Form = Form.Create(new IField[]
             {
-                new LabelField("Coming soon", string.Empty),
-                new ButtonField("Login", new MvxAsyncCommand(HandleLoginCommand)) 
+                _emailAddressField,
+                _passwordField,
+                _buttonField
             });
+        }
+
+        private bool CanHandleLoginCommand()
+        {
+            return _emailAddressField.Value.IsValidEmailAddress()
+                   && _passwordField.Value.IsSavePassword();
         }
 
         private async Task HandleLoginCommand()
         {
             await Task.Delay(TimeSpan.FromSeconds(2));
+
+            _emailAddressField.Value = string.Empty;
+            _passwordField.Value = string.Empty;
         }
     }
 }
